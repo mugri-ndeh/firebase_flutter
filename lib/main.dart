5cb68b80f9e7ff855b390a2d83b6532cf79caa3d
mugri-ndeh <frunwindeh@yahoo.com>
2022-02-22 20:09:15 +0100

    name change

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
-import 'package:firebase_project/cubit/counter_cubit.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+
+import 'cubit/counter_cubit/counter_cubit.dart';
 
 void main() {
   runApp(MyApp());
